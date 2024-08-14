0378a5f58be3a977fa41a7489b72ceb454827beb
Reguide Wiki (Blockchain) <127845264+ReguideWIKI@users.noreply.github.com>
2024-08-14 08:35:10 +0700

    Update allora2.sh

--- allora2.sh
+++ allora2.sh
@@ -183,16 +183,13 @@ cat <<EOF > config.json
 }
 EOF
 echo -e "${BOLD}${UNDERLINE}${DARK_YELLOW} If docker not run when init done, try this ...${RESET}"
-echo -e "RUN: docker compose up --build -d"
-echo -e "VIEW LOGS: docker compose logs -f"
-
 execute_with_prompt 'chmod +x init.config'
-execute_with_prompt './init.config'
-wait
 
-echo -e "${BOLD}${UNDERLINE}${DARK_YELLOW}Building and starting Docker containers...${RESET}"
-docker compose up --build -d
+echo
+echo -e "==============RUN: cd basic-coin-prediction-node"
+echo -e "==============RUN: ./init.config"
+echo -e "==============RUN: docker compose up --build -d"
+echo -e "==============RUN: docker compose up --build -d"
+echo -e "==============VIEW LOGS: docker compose logs -f"
 echo
 
-echo -e "${BOLD}${DARK_YELLOW}Checking running Docker containers...${RESET}"
-docker compose logs -f
