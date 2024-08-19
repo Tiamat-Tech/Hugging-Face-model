27696c4aa0274767268e8060c4dec26064a4e234
Reguide Wiki (Blockchain) <127845264+ReguideWIKI@users.noreply.github.com>
2024-08-19 13:49:32 +0700

    Update upgrade-model.sh

--- upgrade-model.sh
+++ upgrade-model.sh
@@ -15,24 +15,24 @@ if [[ "$installdep" =~ ^[Yy]$ ]]; then
 
     echo -e "${CYAN}Clone & Replace old file :${RESET}"
     echo
-	rm -rf app.py
-	rm -rf requirements.txt
+    rm -rf app.py
+    rm -rf requirements.txt
     wget -q https://raw.githubusercontent.com/ReJumpLabs/Hugging-Face-model/main/app.py -O /root/allora-huggingface-walkthrough/app.py
     wget -q https://raw.githubusercontent.com/ReJumpLabs/Hugging-Face-model/main/requirements.txt -O /root/allora-huggingface-walkthrough/requirements.txt
     wait
 	
-	echo -e "${CYAN}Rebuild and run a model :${RESET}"
+    echo -e "${CYAN}Rebuild and run a model :${RESET}"
 	
-	echo
-    execute_with_prompt 'docker compose down'
+    echo
+    docker compose down
     echo
 	
-	echo
-    execute_with_prompt 'docker compose up --build -d'
+    echo
+    docker compose up --build -d
     echo
 	
-	echo
-    execute_with_prompt 'docker compose logs -f'
+    echo
+    docker compose logs -f
     echo
 	
 else
